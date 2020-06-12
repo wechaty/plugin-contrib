a406929d5663d7a1cd3e4a79b6ea6a48b28a840e
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-13 01:53:26 +0800

    fix plugin typing

--- src/contrib/event-hot-handler.ts
+++ src/contrib/event-hot-handler.ts
@@ -36,7 +36,7 @@ export function EventHotHandler (
     }
   }
 
-  return async function EventHotHandlerPlugin (wechaty: Wechaty) {
+  return function EventHotHandlerPlugin (wechaty: Wechaty): void {
     log.verbose('WechatyPluginContrib', 'EventHotHandler installing on %s ...', wechaty)
 
     for (const key of Object.keys(absolutePathConfig)) {
@@ -44,26 +44,40 @@ export function EventHotHandler (
       const absoluteFilename = absolutePathConfig[eventName]
 
       if (absoluteFilename) {
-        try {
-          const eventHandler = await hotImport(absoluteFilename)
-          wechaty.on(eventName as any, (...args: any[]) => {
-            try {
-              return eventHandler.apply(wechaty, args)
-            } catch (e) {
-              log.error('WechatyPluginContrib', 'EventHotHandler EventHotHandlerPlugin(%s) listener(%s) exception%s',
-                wechaty, eventName, e,
-              )
-              wechaty.emit('error', e)
-            }
-          })
-        } catch (e) {
-          log.error('WechatyPluginContrib', 'EventHotHandler EventHotHandlerPlugin() eventName(%s) hotImport(%s) rejection: %s',
-            eventName, absoluteFilename, e,
-          )
-          wechaty.emit('error', e)
-        }
-
+        addEventHandler(
+          wechaty,
+          eventName,
+          absoluteFilename,
+        ).catch(e => log.error('WechatyPluginContrib', 'EventHotHandler EventHotHandlerPlugin(%s, %s, %s) rejection: %s',
+          wechaty, eventName, absoluteFilename, e,
+        ))
       }
+
     }
+  }
+}
+
+async function addEventHandler (
+  wechaty          : Wechaty,
+  eventName        : WechatyEventName,
+  absoluteFilename : string,
+) {
+  try {
+    const eventHandler = await hotImport(absoluteFilename)
+    wechaty.on(eventName as any, (...args: any[]) => {
+      try {
+        return eventHandler.apply(wechaty, args)
+      } catch (e) {
+        log.error('WechatyPluginContrib', 'EventHotHandler EventHotHandlerPlugin(%s) listener(%s) exception%s',
+          wechaty, eventName, e,
+        )
+        wechaty.emit('error', e)
+      }
+    })
+  } catch (e) {
+    log.error('WechatyPluginContrib', 'EventHotHandler EventHotHandlerPlugin() eventName(%s) hotImport(%s) rejection: %s',
+      eventName, absoluteFilename, e,
+    )
+    wechaty.emit('error', e)
   }
 }
