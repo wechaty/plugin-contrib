744c8d4fea5bb745ec7626a4dbe213c9447037e3
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-13 19:20:03 +0800

    add test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,9 +5,15 @@ import test  from 'tstest'
 import * as contrib from './index'
 
 test('Make sure the module export list is expected', async (t) => {
-  t.ok(contrib.ChatOps, 'should has ChatOps')
-  t.ok(contrib.DingDong, 'should has DingDong')
-  t.ok(contrib.EventLogger, 'should has EventLogger')
-  t.ok(contrib.Heartbeat, 'should has Heartbeat')
-  t.ok(contrib.QRCodeTerminal, 'should has QRCodeTerminal')
+  t.ok(contrib.DingDong, 'should has #1 DingDong')
+  t.ok(contrib.EventLogger, 'should has #2 EventLogger')
+  t.ok(contrib.QRCodeTerminal, 'should has #3 QRCodeTerminal')
+  t.ok(contrib.Heartbeat, 'should has #4 Heartbeat')
+  t.ok(contrib.ChatOps, 'should has #5 ChatOps')
+  t.ok(contrib.OneToManyRoomConnector, 'should has #6.1 OneToManyRoomConnector')
+  t.ok(contrib.ManyToOneRoomConnector, 'should has #6.2 ManyToOneRoomConnector')
+  t.ok(contrib.ManyToManyRoomConnector, 'should has #6.3 ManyToManyRoomConnector')
+  t.ok(contrib.FriendshipAccepter, 'should has #7 FriendshipAccepter')
+  t.ok(contrib.RoomInviter, 'should has #8 RoomInviter')
+  t.ok(contrib.EventHotHandler, 'should has #9 EventHotHandler')
 })
