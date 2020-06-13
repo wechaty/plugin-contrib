3f239818de21f2d8f0d017c600ab538bc55c14ef
Huan LI (李卓桓) <zixia@zixia.net>
2020-06-13 18:42:53 +0800

    add ContactTalker unit test

---
+++ src/utils/contact-talker.spec.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env ts-node
+
+import test  from 'tstest'
+import sinon from 'sinon'
+
+import { Contact } from 'wechaty'
+
+import {
+  contactTalker,
+  ContactTalkerOptions
+}                             from './contact-talker'
+
+test('contactTalker()', async t => {
+  const spy1 = sinon.spy()
+  const spy2 = sinon.spy()
+  const spy3 = sinon.spy()
+  const spy4 = sinon.spy()
+
+  const EXPECTED_TEXT = 'text'
+
+  const OPTIONS_TEXT: ContactTalkerOptions = EXPECTED_TEXT
+  const OPTIONS_FUNCTION: ContactTalkerOptions = spy1
+  const OPTIONS_FUNCTION_LIST: ContactTalkerOptions = [spy2, spy3]
+
+  const mockContact = {
+    say: spy4,
+    wechaty: {
+      sleep: () => undefined,
+    },
+  } as any as Contact
+
+  await contactTalker(OPTIONS_TEXT)(mockContact)
+  t.true(spy4.called, 'should called the contact.say')
+  t.equal(spy4.args[0][0], EXPECTED_TEXT, 'should say the expected text')
+
+  await contactTalker(OPTIONS_FUNCTION)(mockContact)
+  t.true(spy1.called, 'should called the function')
+  t.equal(spy1.args[0][0], mockContact, 'should called the function with contact')
+
+  await contactTalker(OPTIONS_FUNCTION_LIST)(mockContact)
+  t.true(spy2.called, 'should called the functions 1')
+  t.true(spy3.called, 'should called the functions 2')
+  t.equal(spy2.args[0][0], mockContact, 'should called the functions 1 with contact')
+  t.equal(spy3.args[0][0], mockContact, 'should called the functions 2 with contact')
+})
