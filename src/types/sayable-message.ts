8b0a43f45340c99a3f1fee3927060940b066ab39
Huan LI (李卓桓) <zixia@zixia.net>
2020-07-11 07:44:28 +0800

    support number to be said

--- src/types/sayable-message.ts
+++ src/types/sayable-message.ts
@@ -14,6 +14,7 @@ import {
 export type SayableMessage =  undefined
                             | Message
                             | string
+                            | number
                             | FileBox
                             | Contact
                             | UrlLink
