c540bedeb8df6d6dee016394c6b7e76c32c2ecbc
Huan <zixia@zixia.net>
2021-08-11 23:40:52 +0800

    add SourceToTargetRoomConnector

--- src/talkers/room-talker.ts
+++ src/talkers/room-talker.ts
@@ -25,42 +25,58 @@ export function roomTalker<T = void> (options?: RoomTalkerOptions) {
 
   const optionList = options
 
-  return async function talkRoom (room: Room, contact?: Contact, mustacheView?: T): Promise<void> {
+  return async function talkRoom (
+    rooms: Room | Room[],
+    contacts?: Contact | Contact[],
+    mustacheView?: T,
+  ): Promise<void> {
     log.verbose('WechatyPluginContrib', 'roomTalker() talkRoom(%s, %s, %s)',
-      room,
-      contact || '',
+      rooms,
+      contacts || '',
       mustacheView
         ? JSON.stringify(mustacheView)
         : '',
     )
 
-    for (const option of optionList) {
-      let msg
-      if (option instanceof Function) {
-        msg = await option(room, contact)
-      } else {
-        msg = option
-      }
+    if (!Array.isArray(rooms)) {
+      rooms = [rooms]
+    }
+    if (typeof contacts === 'undefined') {
+      contacts = []
+    } else if (!Array.isArray(contacts)) {
+      contacts = [contacts]
+    }
 
-      if (!msg) { continue }
-
-      if (typeof msg === 'string') {
-        if (mustacheView) {
-          msg = Mustache.render(msg, mustacheView)
-        }
-        if (contact) {
-          await room.say(msg, contact)
-        } else {
-          await room.say(msg)
-        }
-      } else {
-        /**
-         *  FIXME(huan): https://github.com/microsoft/TypeScript/issues/14107
-         */
-        await room.say(msg as any)
-      }
-
+    for (const room of rooms) {
+      await loopOptionList(room, contacts)
       await room.wechaty.sleep(1000)
+    }
+
+    async function loopOptionList (room: Room, contactList: Contact[]): Promise<void> {
+      for (const option of optionList) {
+        let msg
+        if (option instanceof Function) {
+          msg = await option(room, ...contactList)
+        } else {
+          msg = option
+        }
+
+        if (!msg) { continue }
+
+        if (typeof msg === 'string') {
+          if (mustacheView) {
+            msg = Mustache.render(msg, mustacheView)
+          }
+          await room.say(msg, ...contactList)
+        } else {
+          /**
+           *  FIXME(huan): https://github.com/microsoft/TypeScript/issues/14107
+           */
+          await room.say(msg as any)
+        }
+
+        await room.wechaty.sleep(1000)
+      }
     }
   }
 }
